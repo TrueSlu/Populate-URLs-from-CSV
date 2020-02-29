1dd9966ada3c783dc53bbf6d64c1b8dc905a197f
Steven Lu <slu.true@gmail.com>
2020-02-28 21:49:51 -0800

    Init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Auto link populater
+
+Requires Node and Yarn.
+
+1. Create `to-populate.txt`. Put a search term per line.
+1. Create `urls.csv`. Add `term,url` in the first line. Add a second line by hitting `Enter` on your keyboard.
+1. Run the `scraper.js` script.
+1. It will search for every term in the list and output the URL of the first results to `urls.csv`
+
+
+Note: obviously, this and my other scrapers aren't perfect, but they aren't meant to be. They're quick, dirty, and get the job done.
---
+++ scraper.js
@@ -0,0 +1,33 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(() => {
+
+    fs.readFile('./to-populate.txt', 'utf8', async function (err, contents) {
+        const searchTerms = contents.split("\n");
+
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        for (var searchTerm of searchTerms) {
+            newSearchTerm = searchTerm.replace(" ", "+");
+
+            await page.goto(`https://www.google.com/search?q=${newSearchTerm}`);
+
+            const result = await page.evaluate(() => {
+
+                const topLink = document.querySelector("#search a").href
+
+                return topLink;
+            }).catch((err) => {
+                console.log(err);
+            });
+
+            console.log(`Found ${result.trim()} for ${searchTerm.trim()}`)
+            fs.appendFileSync('urls.csv', `${searchTerm.trim()},${result.trim()}\n`);
+        }
+        await browser.close();
+    });
+
+
+})();
